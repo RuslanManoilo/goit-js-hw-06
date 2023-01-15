c83d41ce21a4a2783d0134b421d1682082edeabb
Ruslan <manoiloruslan@gmail.com>
2023-01-15 15:33:20 +0200

    task-10.js - complete
    goit-js-hw-06 - complete

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,48 @@
+const inputElem = document.querySelector('input');
+const createElem = document.querySelector('[data-create]');
+const destroyElem = document.querySelector('[data-destroy]');
+const boxesElem = document.querySelector('#boxes');
+// console.dir(inputElem)
+
+
+createElem.addEventListener('click', createBoxes);
+
+function createBoxes() {
+  let numberTest = inputElem.value;
+  let amount = [];
+
+  for (let i = 1; i <= numberTest; i++) {
+    const pushElem = amount.push(i); 
+  }
+
+  // console.log(numberTest);
+  // console.log(amount);
+
+  const markup = amount
+    .map(elem => 
+      `<div 
+        style=
+          "width: ${30 + (elem-1) * 10}px; 
+          height: ${30 + (elem-1) * 10}px;
+          border: ${elem}px solid;
+          background-color: ${getRandomHexColor()};">
+      </div>`)
+    .join("");
+
+  // console.log(markup);
+
+  boxesElem.insertAdjacentHTML("beforeend", markup);
+}
+
+
+
+destroyElem.addEventListener('click', destroyBoxes);
+function destroyBoxes() {
+  boxesElem.innerHTML = "";
+}
+
+
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
